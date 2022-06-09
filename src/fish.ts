5cac78bc985921ef8852bf7f55ec46bf3f5bf47a
TJ-Abbing <tjabbing@gmail.com>
2022-06-09 13:23:47 +0200

    Added keyboard movement

--- src/fish.ts
+++ src/fish.ts
@@ -1,15 +1,24 @@
 import * as PIXI from 'pixi.js'
 
+import { Game } from './game';
 export class Fish extends PIXI.Sprite{
-    constructor(texture: PIXI.Texture){
+    private game: Game;
+    constructor(texture: PIXI.Texture, game: Game){
         super(texture); // new PIXI.Sprite()
+        this.game = game;
         this.anchor.set(0.5);
         this.y = 100;
         this.tint = Math.random() * 0xFF0000;
         this.x = Math.random() * 800;
         this.y = Math.random() * 450;
     }
-    update(delta: number){
+    public update(delta: number){
         this.x -= delta * 1;
+        if(this.x < (0 - this.width)){
+            // finish
+            // this.game.finish();
+            this.x = 800;
+            this.y = Math.random() * 450;
+        }
     }
 }
--- src/game.ts
+++ src/game.ts
@@ -7,24 +7,28 @@ import fishImage from "./images/fish.png"
 import bubbleImage from "./images/bubble.png"
 import waterImage from "./images/water.jpg"
 import { Fish } from './fish';
+import { KeyboardFish } from './keyboardFish';
+
 
 export class Game{
+
     // Canvas size settings
     pixiWidth = 800;
     pixiHeight = 450;
+
     // globals
-    pixi : PIXI.Application;
-    loader : PIXI.Loader;
-    // fish : PIXI.Sprite;
-    fish : Fish;
-    fishes : Fish[];
+    private pixi : PIXI.Application;
+    private loader : PIXI.Loader;
+    private keyboardFish : Fish;
+    private fishes : Fish[];
+
     /**
      * Constructor
      * 
      * Initialize Pixi
      * Load assets
      */
-    constructor(){
+     constructor(){
         this.fishes = [];
         this.pixi = new PIXI.Application({ width: this.pixiWidth, height: this.pixiHeight });
         this.pixi.stage.interactive = true;
@@ -49,28 +53,36 @@ export class Game{
         water.height = this.pixiHeight; // Gives the water the same height as the canvas.
         water.width = this.pixiWidth; // Gives the water the same width as the canvas.
         this.pixi.stage.addChild(water); // Adds the water.
-        this.fish = new Fish(this.loader.resources["fishTexture"].texture!)
-        // this.fish.anchor.set(0.5)
-        this.pixi.stage.addChild(this.fish)
+
         for(let i = 0; i < 5; i++){
-            let temp = new Fish(this.loader.resources["fishTexture"].texture!);
+            let temp = new Fish(this.loader.resources["fishTexture"].texture!, this);
             this.pixi.stage.addChild(temp);
             this.fishes.push(temp);
         }
+
+        this.keyboardFish = new KeyboardFish(this.loader.resources["fishTexture"].texture!, this);
+        this.pixi.stage.addChild(this.keyboardFish)
         
         this.pixi.ticker.add((delta)=>this.update(delta));
     }
-    /**
-     * Update
-     * @param delta 
-     * 
-     * Updates fishes & bubbles
-     */
-    update(delta: number){
-        this.fish.update(delta);
+    private update(delta: number){
+        this.keyboardFish.update(delta);
         for(let f = 0; f < this.fishes.length; f++){
             this.fishes[f].update(delta);
+            if(this.collision(this.keyboardFish, this.fishes[f])){
+                console.log("keyboard fish touches normal fish 💀")
+            }
         }
+
+    }
+
+    private collision(sprite1:PIXI.Sprite, sprite2:PIXI.Sprite) {
+        const bounds1 = sprite1.getBounds()
+        const bounds2 = sprite2.getBounds()
+        return bounds1.x < bounds2.x + bounds2.width
+            && bounds1.x + bounds1.width > bounds2.x
+            && bounds1.y < bounds2.y + bounds2.height
+            && bounds1.y + bounds1.height > bounds2.y;
     }
 }
 new Game();
