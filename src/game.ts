ec37ca7ae4badf0d1189c8b5c8ce566406c2960d
TJ-Abbing <tjabbing@gmail.com>
2022-06-09 12:43:31 +0200

    Class test and changed comments + title

--- src/game.ts
+++ src/game.ts
@@ -1,27 +1,39 @@
+console.log(`Running pixi.js..`) // FIRST LOG
+
+// Imports
+console.log('Running imports..')
 import * as PIXI from 'pixi.js'
 import fishImage from "./images/fish.png"
 import bubbleImage from "./images/bubble.png"
 import waterImage from "./images/water.jpg"
 
-//
-// STAP 1 - maak een pixi canvas
-//
+class Test {
+    test = true
+}
+
+let test = new Test()
+
+console.log(`The test is ${test.test}`)
+
+// Creates pixi canvas
+console.log('Creating pixi canvas..')
 const pixi = new PIXI.Application({ width: 800, height: 450 })
 document.body.appendChild(pixi.view)
 
-//
-// STAP 2 - preload alle afbeeldingen
-//
+
+// Preloads all images
+console.log(`Preloading images..`)
 const loader = new PIXI.Loader()
 loader.add('fishTexture', fishImage)
       .add('bubbleTexture', bubbleImage)
       .add('waterTexture', waterImage)
 loader.load(()=>loadCompleted())
 
-//
-// STAP 3 - maak een sprite als de afbeeldingen zijn geladen
-//
+// Creates sprite when images are loaded
+console.log(`Creatings sprites..`)
 function loadCompleted() {
     let fish = new PIXI.Sprite(loader.resources["fishTexture"].texture!)
     pixi.stage.addChild(fish)
 }
+
+console.log(`Finished pixi.js.`) // LAST LOG
